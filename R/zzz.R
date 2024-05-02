42ed1e44e133223ab6e067dfcfe9e3b6e4b3a0ae
lemuscanovas <marc_lemus@hotmail.com>
2024-05-02 09:32:57 +0200

    package startup message updated

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0(c("\n****\nWelcome to synoptReg! \n",
-                                 "Using synoptReg for research publication?  Please cite it!\n",
-                                 "I'm an early career researcher and every citation matters.\n****\n")))}
+  packageStartupMessage(paste0(c("\n****\nWelcome to climattR! \n",
+                                 "Using climattR for scientific publication?  Please cite it!\n")))}
